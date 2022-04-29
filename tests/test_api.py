de85f511f0fac5d063faea8a46b6e2ba81ec3533
Baptiste Fontaine <baptiste@bixoto.com>
2022-04-29 11:11:38 +0200

    remove old code

--- tests/test_api.py
+++
@@ -1,9 +0,0 @@
-from bigbuy import BigBuy
-
-KEY = 'YmU4NzU4ZTEwMWQzOTlmNzAwMWMxZmY0N2E3MGVhYzFhZTBlMzFkOTdlOGJjOTY0OTQ1MGZlYzg3NDkxZDc0ZA'
-
-
-def test_check_order():
-    bb = BigBuy(app_key=KEY, mode="sandbox")
-    # FIXME: this payload needs to be filled to work
-    # assert bb.check_order({}) is not None
