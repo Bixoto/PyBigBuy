ecf6e7426955ff68ee7c11ebd79634b9abcda9a4
Baptiste Fontaine <baptiste@bixoto.com>
2026-01-06 09:57:13 +0100

    endpoints_coverage.sh: exclude categories

--- endpoints_coverage.sh
+++ endpoints_coverage.sh
@@ -6,23 +6,24 @@
 rm -f endpoints.txt supported.txt
 
 # Grep everything that looks like an endpoint
-# Exclude 'module/platforms' (the route is not documented) and the false positives 'application/…' and '/rest/order/…'
+# Exclude the false positives 'application/…' and '/rest/order/…'
 grep -Eo "['\"][^/][^'\":]+/[^'\" ]+['\"]" bigbuy/api.py \
   | grep -v application/json \
   | grep -v application/pdf \
   | sed 's%{[^}]*}%{placeholder}%' \
   | sed -E "s%['\"]%%g" \
-  | grep -v '^module/platforms$' \
   | grep -v '\\\\/order\\\\/12[34]' \
   | sort \
   >| supported.txt
 
 # Do the same using the BigBuy doc
+# Exclude the category/categories routes, as they are deprecated
 curl -s http://api.bigbuy.eu/rest/doc.json \
   | jq -r '.paths|to_entries[].key' \
   | sed 's%/rest/%%' \
   | sed "s%\.{format}%%" \
   | sed 's%{[^}]*}%{placeholder}%' \
+  | grep -v 'catalog/categor' \
   | sort \
   >| endpoints.txt
 
